095e168c866265a39b53d9d2c6d66cb59750f833
JoaoFilho33 <joaod.almeidafi@gmail.com>
2023-05-25 12:42:35 -0300

    Passo a passo do algoritmos de Morris in-order

--- algoritmo-de-morris/atividade2_dia_23-05/pre-order.cpp
+++ algoritmo-de-morris/atividade2_dia_23-05/pre-order.cpp
@@ -35,15 +35,21 @@ C) Aplique a árvore no algoritmo e confira se o resultado está correto.
 -
 .Inicializa "p" como 10.
 .O nó atual "p" tem um filho esquerdo, então buscamos o nó mais à direita na subárvore esquerda de "p", que é 7.
-	.Como o nó 7 não tem um ponteiro direito definido, definimos o ponteiro direito do nó 7 para apontar para "p" (7 -> 10) e visitamos o nó 10. Em seguida, atualizamos "p" para o nó esquerdo, ou seja, p = 5.
+	.Como o nó 7 não tem um ponteiro direito definido, definimos o ponteiro direito 
+        do nó 7 para apontar para "p" (7 -> 10) e visitamos o nó 10. Em seguida, 
+        atualizamos "p" para o nó esquerdo, ou seja, p = 5.
 .O nó atual "p" tem um filho esquerdo, então buscamos o nó mais à direita na subárvore esquerda de "p", que é 3.
-	.Como o nó 3 não tem um ponteiro direito definido, definimos o ponteiro direito do 	nó 3 para apontar para "p" (3 -> 5) e visitamos o nó 5. Em seguida, atualizamos "p" para o nó esquerdo, ou seja, p = 3.
+	.Como o nó 3 não tem um ponteiro direito definido, definimos o ponteiro direito 
+        do 	nó 3 para apontar para "p" (3 -> 5) e visitamos o nó 5. Em seguida, 
+        atualizamos "p" para o nó esquerdo, ou seja, p = 3.
 .O nó atual "p" não tem um filho esquerdo. Portanto, visitamos o nó 3 e atualizamos "p" para o nó direito, ou seja, p = 5.
 .O nó atual "p" não tem um filho esquerdo. Portanto, visitamos o nó 5 e atualizamos "p" para o nó direito, ou seja, p = 7.
 .O nó atual "p" não tem um filho esquerdo. Portanto, visitamos o nó 7 e atualizamos "p" para o nó direito, ou seja, p = 10.
 .O nó atual "p" não tem um filho esquerdo. Portanto, visitamos o nó 10 e atualizamos "p" para o nó direito, ou seja, p = 20.
 .O nó atual "p" tem um filho esquerdo. Portanto, buscamos o nó mais à direita na subárvore esquerda de "p", que é 15.
-	.Como o nó 15 não tem um ponteiro direito definido, definimos o ponteiro direito  do nó 15 para apontar para "p" (15 -> 20) e visitamos o nó 20. Em seguida, atualizamos "p" para o nó esquerdo, ou seja, p = 15.
+	.Como o nó 15 não tem um ponteiro direito definido, definimos o ponteiro direito  
+        do nó 15 para apontar para "p" (15 -> 20) e visitamos o nó 20. Em seguida, 
+        atualizamos "p" para o nó esquerdo, ou seja, p = 15.
 .O nó atual "p" não tem um filho esquerdo. Portanto, visitamos o nó 15 e atualizamos "p" para o nó direito, ou seja, p = 20.
 .O nó atual "p" não tem um filho esquerdo. Portanto, visitamos o nó 20 e atualizamos "p" para o nó direito, ou seja, p = 24.
 .O nó atual "p" não tem um filho esquerdo. Portanto, visitamos o nó 24 e atualizamos "p" para nulo.
